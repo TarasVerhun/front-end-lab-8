dc42965f49723ccf17ce5c536b9d8ccedbc87c77
Taras Verhun <verhun.taras@gmail.com>
2018-02-10 02:49:46 +0200

    own functions are forbidden to use

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,32 +1,28 @@
-function pyramid(height) {
-    let elem = '[~]';
-    let elem1 = elem;
-    let space = '   ';
-    let p = '';
-    let row;
-    for (let i = 0; i < height; i++) {
-        let str = "";
-        for (let j = height - 1; j > i; j--) {
-            str = str.concat(space);
-        }
-        row = str.concat(elem.concat(str));
-        elem = elem.concat(elem1.concat(elem1));
-        p = p.concat(row).concat('\n');
-    }
-    console.log(p);
-}
-
-function isInteger(num) {
-    return num % 1 == 0;
-}
-
-let height = parseFloat(prompt('Enter the number between 1 and 20'));
-if (!isInteger(height)) {
-    console.error('Just enter the NATURAL number, please!');
+let height = prompt('Enter the natural number between 1 and 20');
+if (isNaN(height) || !height) {
+    console.error("Please enter the NUMBER");
 } else {
-    if (height > 0 && height <= 20) {
-        pyramid(height);
+    if (!Number.isInteger(parseFloat(height))) {
+        console.error('Just enter the NATURAL number, please!');
     } else {
-        console.error('The number must match required range: (1-20).');
+        if (height > 0 && height <= 20) {
+            let elem = "[~]";
+            let elem1 = elem;
+            let space = "   ";
+            let p = "";
+            let row;
+            for (let i = 0; i < height; i++) {
+                let str = "";
+                for (let j = height - 1; j > i; j--) {
+                    str = str.concat(space);
+                }
+                row = str.concat(elem.concat(str));
+                elem = elem.concat(elem1.concat(elem1));
+                p = p.concat(row).concat("\n");
+            }
+            console.log(p);
+        } else {
+            console.error('The number must match required range: (1-20).');
+        }
     }
 }
--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,58 +1,91 @@
-const min = 0;
 let max = 5;
 let prizes = [10, 5, 2];
 let total = 0;
+let accept = true;
+let offer = true;
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function offerGame() {
-    prizes = [10, 5, 2];
-    total = 0;
-    max = 5;
-    let agree = confirm("Do you want to play a game?");
+while (accept) {
+    if (offer) {
+        agree = confirm("Do you want to play a game?");
+    }
     if (!agree) {
         console.log("You did not become a millionaire");
+        break;
     } else {
-        play();
+        let generatedNumber = Math.floor(Math.random() * (max + 1));
+        let count = 0;
+        let currentAward;
+        for (let i = 0; i < 3; i++) {
+            count++;
+            let userNumber = parseInt(
+                prompt(
+                    "Enter a number from 0 to " +
+                    max +
+                    "\n" +
+                    "Attemts left: " +
+                    (3 - i) +
+                    "\n" +
+                    "Total prize: " +
+                    total +
+                    "$\n" +
+                    "Possible prize in current attempt: " +
+                    prizes[i] +
+                    "$"
+                )
+            );
+            if (userNumber === generatedNumber) {
+                total += prizes[i];
+                currentAward = prizes[i];
+                prizes.forEach((el, i) => {
+                    i == 0 ?
+                        (prizes[i] = el * 3) :
+                        (prizes[i] = Math.floor(prizes[i - 1] / 2));
+                });
+                max *= 2;
+                console.log("good. it was " + userNumber + ". You are lucky!");
+                count--;
+                break;
+            }
+        }
+        if (count == 3) {
+            offer = false;
+            if (
+                confirm(
+                    "You have lost. The game is over for You. Your prize is " +
+                    total +
+                    "$. Do You want play again?"
+                )
+            ) {
+                prizes = [10, 5, 2];
+                total = 0;
+                max = 5;
+            } else {
+                console.log("Thank you for a game. Your prize is " + total + " $.");
+                accept = false;
+            }
+        } else {
+            if (!confirm(
+                    "You won " +
+                    currentAward +
+                    " $. Your total prize is " +
+                    total +
+                    "$. Do you wont to continue?"
+                )) {
+                if (confirm("Do you want to play from the beginning again?")) {
+                    offer = false;
+                    console.log(
+                        "In this game Your prize is " + total + " $. Let's start again."
+                    );
+                    max = 5;
+                    prizes = [10, 5, 2];
+                    total = 0;
+                } else {
+                    accept = false;
+                    console.log("Thank you for a game. Your prize is " + total + " $.");
+                }
+            } else {
+                offer = false;
+            }
+        }
     }
 }
-
-function play() {
-    let generatedNumber = getRandomInt(min, max);
-    let count = 0;
-    let currentAward;
-    for (let i = 0; i < 3; i++) {
-        count++;
-        let userNumber = parseInt(prompt("Enter a number from " + min + " to " + max + "\n" + "Attemts left: " + (3 - i) + "\n" + "Total prize: " + total + "$\n" + "Possible prize in current attempt: " + prizes[i] + "$"));
-        if (userNumber === generatedNumber) {
-            total += prizes[i];
-            currentAward = prizes[i];
-            prizes.forEach((el, i) => prizes[i] = el * 3);
-            max *= 2;
-            console.log("good. it was " + userNumber + '. You are lucky!');
-            count--;
-            break;
-        }
-    }
-    if (count == 3) {
-        if (confirm("You have lost. The game is over for You. Your prize is " + total + "$. Do You want play again?")) {
-            prizes = [10, 5, 2];
-            total = 0;
-            max = 5;
-            play();
-        } else {
-            console.log("Thank you for a game. Your prize is " + total + " $.");
-        }
-    } else {
-        if (confirm("You won " + currentAward + " $. Your total prize is " + total + "$. Do you wont to continue?")) {
-            play();
-        } else {
-            console.log("Thank you for a game. Your prize is " + total + " $.");
-            offerGame();
-        }
-    }
-}
-
-offerGame();
