edc12e4e5c3677f4bb0b59d039641812f9dbb7fa
Taras Verhun <verhun.taras@gmail.com>
2018-02-17 23:14:40 +0200

    one stroke function body

--- homework_08/js/getMin.js
+++ homework_08/js/getMin.js
@@ -1,7 +1,11 @@
 function getMin() {
     let arr = [];
-    for(let i = 0; i<arguments.length;i++){
+    for (let i = 0; i < arguments.length; i++) {
         arr.push(arguments[i]);
     }
-    return arr.sort((a,b)=>a-b)[0];
+    return arr.sort((a, b) => a - b)[0];
 }
+
+// function getMin() {
+//     return Array.prototype.slice.call(arguments).sort((a, b) => a - b)[0];
+// }
